53f56e11a27d8c1e7063fa7266918f64d5318471
imran <imran8122005@gmail.com>
2026-03-25 21:11:07 +0530

    fixed internet issue

--- lib/screens/preview_webview_native.dart
+++ lib/screens/preview_webview_native.dart
@@ -1,6 +1,9 @@
-// Native implementation: renders HTML using webview_flutter
+// Native implementation: renders HTML using webview_flutter + local HTTP server
 import 'dart:convert';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as shelf_io;
 import 'package:webview_flutter/webview_flutter.dart';
 import '../services/storage_service.dart';
 
@@ -34,24 +37,53 @@ class _NativePreviewState extends State<_NativePreview> {
   late WebViewController _controller;
   late StorageService _storage;
   bool _isLoading = true;
+  HttpServer? _server;
+  int _port = 0;
 
   @override
   void initState() {
     super.initState();
     _storage = StorageService(widget.appId);
-    _initController();
+    _startServerAndInit();
   }
 
-  Future<void> _initController() async {
+  @override
+  void dispose() {
+    _server?.close(force: true);
+    super.dispose();
+  }
+
+  Future<void> _startServerAndInit() async {
     // Pre-load all storage items for synchronous JS access
     final items = await _storage.getAllItems();
     final storageJson = jsonEncode(items);
+    final injectedHtml = _injectScripts(widget.html, storageJson);
+
+    // Start a local HTTP server on a random available port
+    final handler = const shelf.Pipeline().addHandler((shelf.Request request) {
+      return shelf.Response.ok(
+        injectedHtml,
+        headers: {
+          'Content-Type': 'text/html; charset=utf-8',
+          // Allow the page to fetch any origin
+          'Access-Control-Allow-Origin': '*',
+        },
+      );
+    });
+
+    _server = await shelf_io.serve(handler, InternetAddress.loopbackIPv4, 0);
+    _port = _server!.port;
+    debugPrint('Preview server running on http://127.0.0.1:$_port');
 
     _controller = WebViewController()
       ..setJavaScriptMode(JavaScriptMode.unrestricted)
       ..setNavigationDelegate(NavigationDelegate(
         onPageFinished: (_) {
           if (mounted) setState(() => _isLoading = false);
+        },
+        onNavigationRequest: (NavigationRequest request) {
+          // Let the preview page navigate freely (opens links, etc.)
+          return NavigationDecision.navigate;
         },
       ))
       // Console capture channel
@@ -85,8 +117,7 @@ class _NativePreviewState extends State<_NativePreview> {
           _storage.clear();
         },
       )
-      ..loadHtmlString(
-          _injectScripts(widget.html, storageJson));
+      ..loadRequest(Uri.parse('http://127.0.0.1:$_port/'));
 
     if (mounted) setState(() {});
   }
@@ -163,14 +194,18 @@ class _NativePreviewState extends State<_NativePreview> {
 })();
 </script>
 ''';
-    return htmlContent.replaceFirst('</head>', '$script</head>');
+    // Inject before </head>; if no </head>, prepend the script
+    if (htmlContent.contains('</head>')) {
+      return htmlContent.replaceFirst('</head>', '$script</head>');
+    }
+    return '$script$htmlContent';
   }
 
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        WebViewWidget(controller: _controller),
+        if (_port > 0) WebViewWidget(controller: _controller),
         if (_isLoading)
           const Center(
             child: CircularProgressIndicator(color: Color(0xFFFFFFFF)),
